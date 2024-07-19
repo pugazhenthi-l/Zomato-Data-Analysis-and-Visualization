f1b9cf5ef3b46b6a64316c9804fa7bbd961c0052
Pugazhenthi <128439013+pugazhenthi-l@users.noreply.github.com>
2024-07-19 17:47:56 +0530

    basic changes

--- city_analysis.py
+++ city_analysis.py
@@ -24,18 +24,21 @@ def main():
         x='Cuisines',
         y='count',
         title='Famous Cuisine in the City',
-        labels={'index': 'Cuisine', 'Cuisines': 'Cuisines'}
+        labels={'index': 'Cuisine', 'Cuisines': 'Cuisines'},
+        color_discrete_sequence=['#e03546']  # Setting bar color to red
     ))
 
     # Costlier Cuisine in the City
-    st.plotly_chart(px.box(filtered_df.groupby('Cuisines').agg({'Converted Cost (INR)': 'mean'}).reset_index(),
-                           x='Cuisines', y='Converted Cost (INR)', title='Costlier Cuisine in the City'))
+    st.plotly_chart(px.line(filtered_df.groupby('Cuisines').agg({'Converted Cost (INR)': 'mean'}).reset_index(),
+                           x='Cuisines', y='Converted Cost (INR)', title='Costlier Cuisine in the City',
+                           color_discrete_sequence=['#e03546']))
 
     # Rating Count in the City
-    st.plotly_chart(px.bar(filtered_df, x='Cuisines', y='Votes', title='Rating Count in the City', color='Aggregate rating'))
+    st.plotly_chart(px.bar(filtered_df, x='Cuisines', y='Votes', title='Rating Count in the City', color='Aggregate rating', color_discrete_sequence=['#e03546']))
 
     # Pie Chart Online Delivery vs Dine-In
-    st.plotly_chart(px.pie(filtered_df, names='Has Online delivery', title='Delivery Mode in the City'))
+    st.plotly_chart(px.pie(filtered_df, names='Has Online delivery', title='Delivery Mode in the City',
+                           color='Has Online delivery', color_discrete_map={'Yes': '#e03546', 'No': '#21394f'}))
 
 if __name__ == '__main__':
     main()
